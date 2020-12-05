0aaf01e3adeaf3118e67a4db25fe0ba824ba704b
dkmaverick39 <deepak.k039@gmail.com>
2020-12-05 14:57:03 +0530

    Committing generated model entity from json

--- quora-api/src/main/java/com/upgrad/quora/api/QuoraApiApplication.java
+++ quora-api/src/main/java/com/upgrad/quora/api/QuoraApiApplication.java
@@ -3,13 +3,14 @@ package com.upgrad.quora.api;
 import com.upgrad.quora.service.ServiceConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.Import;
 
 /**
  * A Configuration class that can declare one or more @Bean methods and trigger auto-configuration and component scanning.
  * This class launches a Spring Application from Java main method.
  */
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 @Import(ServiceConfiguration.class)
 public class QuoraApiApplication {
     public static void main(String[] args) {
