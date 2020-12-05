11cb5d7ca24200efd2168a1d8cc158df89d431a8
dkmaverick39 <deepak.k039@gmail.com>
2020-12-05 16:02:45 +0530

    Removing exclude data source config

--- quora-api/src/main/java/com/upgrad/quora/api/QuoraApiApplication.java
+++ quora-api/src/main/java/com/upgrad/quora/api/QuoraApiApplication.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Import;
  * A Configuration class that can declare one or more @Bean methods and trigger auto-configuration and component scanning.
  * This class launches a Spring Application from Java main method.
  */
-@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+@SpringBootApplication
 @Import(ServiceConfiguration.class)
 public class QuoraApiApplication {
     public static void main(String[] args) {
